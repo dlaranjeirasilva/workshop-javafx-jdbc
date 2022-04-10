65ebe07fab74c12b091940cc1cc0c58aa2f742a4
Douglas Laranjeira da Silva <douglas.silva@radixeng.com.br>
2022-04-10 14:05:19 -0300

    DepartmentForm (dialog) Design

---
+++ src/gui/util/Utils.java
@@ -0,0 +1,16 @@
+package gui.util;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+	
+	public static Stage currentStage(ActionEvent evt) {
+		return (Stage) ((Node) evt.getSource()).getScene().getWindow();
+	}
+	
+	
+
+	
+}
